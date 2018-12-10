7f9b55322bc0bb730f878d37f3fbe207be61d200
Yuantao315926693 <315926693@qq.com>
2018-12-10 15:25:37 +0800

    frist

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,9 +5,10 @@ const app = getApp()
 Page({
     data: {
         imgUrls: [
-            'https://topic.camel.com.cn/module/wap_index_focus_201709/7645c28f-9674-49f0-8490-dca3f6237496.jpg?d=1',
-            'https://topic.camel.com.cn/module/wap_index_focus_201709/cb445730-dda3-466e-ab17-60f1f6ca1cb7.jpg',
-            'https://topic.camel.com.cn/module/wap_index_focus_201709/92b890bb-7182-4ba4-8bc7-3ffd7ebbcf1c.jpg'
+          // 切换
+            // 'https://topic.camel.com.cn/module/wap_index_focus_201709/7645c28f-9674-49f0-8490-dca3f6237496.jpg?d=1',
+          'http://img.hb.aicdn.com/877bbe49c17ee06f4203ef937598841fb316eaa447a9b-Rk30sf_fw658', 'http://img.hb.aicdn.com/00c6a070b78d34b1b944a6455f11365e961479c1b57c6-DP8EV0_fw658',
+            'http://img.hb.aicdn.com/b7401b461d82f02553352825db7970036746e77351540-ZGokz4_fw658'
         ],
         indicatorDots: true,  
         indicatorColor: '#fff',
