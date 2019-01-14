ede3311fe107dbe050bd54e6976f1c4b882e8d4c
Yuantao315926693 <315926693@qq.com>
2019-01-14 16:32:17 +0800

    yt

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,7 +7,7 @@ Page({
         imgUrls: [
           // 切换
           
-          'https://raw.githubusercontent.com/imlosedog/imlosedog.github.io/master/%E6%A8%AA%E7%89%881.jpg', 'http://img.hb.aicdn.com/00c6a070b78d34b1b944a6455f11365e961479c1b57c6-DP8EV0_fw658',
+          'https://raw.githubusercontent.com/imlosedog/imlosedog.github.io/master/%E6%A8%AA%E7%89%881.jpg', 'https://raw.githubusercontent.com/imlosedog/imlosedog.github.io/master/%E6%A8%AA%E7%89%882.jpg',
             'http://img.hb.aicdn.com/b7401b461d82f02553352825db7970036746e77351540-ZGokz4_fw658'
         ],
         indicatorDots: true,  
