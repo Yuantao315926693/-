bdce98eac4ffabd6b4802080cb74dbb850ff3eb8
imlosedog <1145250434@qq.com>
2019-01-14 17:35:43 +0800

    nz

--- pages/index/index.js
+++ pages/index/index.js
@@ -6,8 +6,7 @@ Page({
     data: {
         imgUrls: [
           // 切换
-          'https://raw.githubusercontent.com/imlosedog/imlosedog.github.io/master/%E6%A8%AA%E7%89%881.jpg', 
-          'https://raw.githubusercontent.com/imlosedog/imlosedog.github.io/master/%E6%A8%AA%E7%89%882.jpg','https://raw.githubusercontent.com/imlosedog/imlosedog.github.io/master/%E6%A8%AA%E7%89%883.jpg'
+          'https://raw.githubusercontent.com/imlosedog/imlosedog.github.io/master/%E6%A8%AA%E7%89%881.jpg',           'https://raw.githubusercontent.com/imlosedog/imlosedog.github.io/master/%E6%A8%AA%E7%89%882.jpg','https://raw.githubusercontent.com/imlosedog/imlosedog.github.io/master/%E6%A8%AA%E7%89%883.jpg'
         ],
         indicatorDots: true,  
         indicatorColor: '#fff',
