f78c78aec8c8f864a92480be2d6b37e503b4b15d
imlosedog <1145250434@qq.com>
2019-01-17 17:37:34 +0800

    nz

--- pages/basket/basket.js
+++ pages/basket/basket.js
@@ -5,8 +5,31 @@ Page({
    * 页面的初始数据
    */
   data: {
+    t_num: 2,
+    t_price: 0.00,
     boolean: false,
-    mun :2
+    p_data:[
+
+      {
+        id: 1,
+        p_info:"这是测试数据1",
+        p_size:"180(黑色)",
+        price:10.00,
+        p_able: 0,
+        p_num:1
+        
+      },
+      {
+        id: 2,
+        p_info: "这是测试数据2",
+        p_size: "170(白色)",
+        price: 20.00,
+        p_able:1,
+        p_num:2
+       
+      }
+    ]
+    
 
 
   },
@@ -15,21 +38,22 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+
+   
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+    
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+    this.fin_Price()
   },
 
   /**
@@ -76,7 +100,86 @@ Page({
       boolean: !bol
 
     })
-  }
+  },
 
- 
+//购物车内商品展示框
+
+//商品选择
+p_clecked: function (e) {
+   console.log(e)
+  var pid=e.currentTarget.dataset.index
+  pid--;
+  console.log(pid)
+  var able = !e.currentTarget.dataset.able
+  console.log(able)
+  var up = "p_data[" + pid + "].p_able";//先用一个变量，把(info[0].gMoney)用字符串拼接起来
+  console.log(up)
+  this.setData({
+  
+   [up]:able
+
+  })
+  this.fin_Price()
+  },
+
+  //商品数量减
+p_decnum:function(e)
+{
+  var pid = e.currentTarget.dataset.index
+  pid--;
+  var pnum = e.currentTarget.dataset.num
+  if(pnum>1)
+  pnum--;
+  console.log(pnum)
+  var up = "p_data[" + pid + "].p_num";//先用一个变量，把(info[0].gMoney)用字符串拼接起来
+  this.setData({
+
+    [up]: pnum
+
+  })
+  this.fin_Price()
+},
+  p_addnum: function (e) {
+    var pid = e.currentTarget.dataset.index
+    pid--;
+    var pnum = e.currentTarget.dataset.num
+    //可能的限制条件
+      pnum++;
+    console.log(pnum)
+    var up = "p_data[" + pid + "].p_num";//先用一个变量，把(info[0].gMoney)用字符串拼接起来
+    
+    
+
+    this.setData({
+
+      [up]: pnum,
+      
+
+    })
+    this.fin_Price()
+  },
+
+//计算最终价格
+  fin_Price: function () {
+    var num = this.data.t_num
+    var price = 0
+    console.log(price)
+    var p_data = this.data.p_data
+    console.log(p_data[0])
+    console.log(p_data[0].p_able)
+    console.log(p_data[1].p_able)
+    for (var i = 0; i < num; i++)
+    {
+      if(p_data[i].p_able)
+      price += (p_data[i].p_num * p_data[i].price);
+    }
+    console.log(price)
+    this.setData({
+
+      t_price: price
+
+    })
+
+}
+
 })
